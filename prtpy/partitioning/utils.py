9bc7067d9020f84edb18cd16581bee62f3fe7600
samuelharroch <samuel@optival.com>
2022-05-31 18:26:23 +0300

    In_ex_tree performance improving

--- prtpy/partitioning/utils.py
+++ prtpy/partitioning/utils.py
@@ -2,18 +2,22 @@
 Authors: Jonathan Escojido & Samuel Harroch
 
 Since = 03-2022
+
+We performed this module by keeping sums in Node fields instead of calculating every time
+
 """
 
 from typing import List, Generator, Callable
 
 
 class Node:
-    def __init__(self,depth, cur_set, remaining_numbers):
+    def __init__(self,depth, cur_set, cur_set_sum, remaining_sums):
         self.depth = depth
         self.cur_set = cur_set
-        self.remaining_numbers = remaining_numbers
+        self.cur_set_sum = cur_set_sum
         self.left = None
         self.right = None
+        self.remaining_sums = remaining_sums
 
 
 class InExclusionBinTree:
@@ -21,22 +25,26 @@ class InExclusionBinTree:
     def __init__(self, items: List, valueof: Callable, upper_bound, lower_bound):
         self.items = sorted(items, key=valueof, reverse=True)
         self.leaf_depth = len(items)
-        self.root = Node(0, [], self.items)  # root
         self.valueof = valueof
+        self.total_sum = sum(map(self.valueof, self.items))
         self.upper_bound = upper_bound
         self.lower_bound = lower_bound
+        self.root = Node(0, [], 0, self.total_sum)  # root
 
     # inclusion
     def add_right(self, parent: Node):
+        item_to_add = self.items[parent.depth]
         parent.right = Node(depth=parent.depth+1,
-                            cur_set=parent.cur_set + [parent.remaining_numbers[0]],
-                            remaining_numbers=parent.remaining_numbers[1:])
+                            cur_set=parent.cur_set + [item_to_add],
+                            cur_set_sum=parent.cur_set_sum + self.valueof(item_to_add),
+                            remaining_sums=parent.remaining_sums - self.valueof(item_to_add))
 
     # exclusion
     def add_left(self, parent: Node):
         parent.left = Node(depth=parent.depth+1,
                            cur_set=parent.cur_set,
-                           remaining_numbers=parent.remaining_numbers[1:])
+                           cur_set_sum=parent.cur_set_sum,
+                           remaining_sums=parent.remaining_sums)
 
     def generate_tree(self) -> Generator:
         current_node = self.root
@@ -44,8 +52,8 @@ class InExclusionBinTree:
 
     def rec_generate_tree(self, current_node: Node) -> Generator:
         # prune
-        if sum(map(self.valueof,current_node.cur_set)) > self.upper_bound or \
-                sum(map(self.valueof,current_node.cur_set + current_node.remaining_numbers)) < self.lower_bound:
+        if current_node.cur_set_sum > self.upper_bound or \
+                (current_node.cur_set_sum + current_node.remaining_sums) < self.lower_bound:
             return
         # generate
         if current_node.depth == self.leaf_depth:
